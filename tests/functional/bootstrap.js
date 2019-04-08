097ff4e0bf8c6586b31111166cdeb9e5e2249140
Roderick Hsiao <roderickhsiao@users.noreply.github.com>
2019-04-08 15:47:29 -0700

    Fix CJS default export (#228)
    * Fix CJS default export

--- tests/functional/bootstrap.js
+++ tests/functional/bootstrap.js
@@ -10,10 +10,10 @@ window.ReactDOM = require('react-dom');
 // TODO, this should be deprecated
 window.createClass = require('create-react-class');
 
-window.I13nAnchor = require('../../dist/components/I13nAnchor').default;
-window.I13nButton = require('../../dist/components/I13nButton').default;
-window.I13nDiv = require('../../dist/components/I13nDiv').default;
+window.I13nAnchor = require('../../dist/components/I13nAnchor');
+window.I13nButton = require('../../dist/components/I13nButton');
+window.I13nDiv = require('../../dist/components/I13nDiv');
 
-window.createI13nNode = require('../../dist/utils/createI13nNode').default;
-window.clickHandler = require('../../dist/libs/clickHandler').default;
-window.setupI13n = require('../../dist/utils/setupI13n').default;
+window.createI13nNode = require('../../dist/utils/createI13nNode');
+window.clickHandler = require('../../dist/libs/clickHandler');
+window.setupI13n = require('../../dist/utils/setupI13n');
