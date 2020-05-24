effeb9cff89246c050072084e7d7a7ba3ce3071e
Roderick Hsiao <roderickhsiao@gmail.com>
2020-05-24 16:14:56 -0700

    Time machine

--- src/libs/DebugDashboard.js
+++ src/libs/DebugDashboard.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2020 - Present, Yahoo Inc.
+ * Copyright 2015 -  Present, Yahoo Inc.
  * Copyrights licensed under the New BSD License. See the accompanying LICENSE file for terms.
  */
 /* global document, window */
--- src/libs/clickHandler.js
+++ src/libs/clickHandler.js
@@ -1,6 +1,6 @@
 import isUndefined from '../utils/isUndefined';
 /**
- * Copyright 2020 - Present, Yahoo Inc.
+ * Copyright 2015 -  Present, Yahoo Inc.
  * Copyrights licensed under the New BSD License. See the accompanying LICENSE file for terms.
  */
 /* globals document, window */
