1d30a6af9027fb59eccbdc0381445713324f5a3e
Chris Santamaria <chris@santamaria.me>
2021-04-28 20:58:39 -0400

    Read variables from process.env directly

--- src/utils/variables.js
+++ src/utils/variables.js
@@ -4,10 +4,8 @@
  */
 import isUndefined from './isUndefined';
 
-const { NODE_ENV } = process.env;
-
-export const IS_PROD = NODE_ENV === 'production';
-export const IS_TEST = NODE_ENV === 'test';
+export const IS_PROD = process.env.NODE_ENV === 'production';
+export const IS_TEST = process.env.NODE_ENV === 'test';
 export const IS_CLIENT = typeof window !== 'undefined';
 export const ENVIRONMENT = IS_CLIENT ? 'client' : 'server';
 
