31db1bf63154446fb0ffe6fe6d54cf1f3f4f62cf
Chris Santamaria <chris@santamaria.me>
2022-03-29 19:29:29 -0400

    fix: only call useLayoutEffect client-side (#552)

--- src/hooks/useDebugDashboard.js
+++ src/hooks/useDebugDashboard.js
@@ -3,17 +3,18 @@
  * Copyrights licensed under the New BSD License. See the accompanying LICENSE file for terms.
  */
 
-import { useLayoutEffect, useRef } from 'react';
+import { useRef } from 'react';
 
 import { IS_DEBUG_MODE } from '../utils/variables';
 import DebugDashboard from '../libs/DebugDashboard';
+import useIsomorphicLayoutEffect from './useIsomorphicLayoutEffect';
 
 const useDebugDashboard = ({
   node
 }) => {
   const debugDashboard = useRef();
 
-  useLayoutEffect(() => {
+  useIsomorphicLayoutEffect(() => {
     if (IS_DEBUG_MODE) {
       debugDashboard.current = new DebugDashboard(node);
     }
